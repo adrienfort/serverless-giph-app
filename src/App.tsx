778b0eb47b9feacf90b5b0c4b993462c31ed859d
Adrien FORT <adrien.fort01@gmail.com>
2021-07-21 19:13:38 +0200

    feat: use yarn and typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export default function App(): JSX.Element {
+	return (
+		<Box textAlign="center" fontSize="xl" w="100%" h="100%">
+			<Text>Ramify</Text>
+		</Box>
+	);
+}
