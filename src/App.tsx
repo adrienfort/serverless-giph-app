645c920c6ef6533c0eb40c27617a43a25900087b
Adrien FORT <adrien.fort01@gmail.com>
2021-07-29 00:05:09 +0200

    fix: remove useless center div

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { Box, Center, Grid, Input, Text } from '@chakra-ui/react';
+import { Flex, Grid, Input, Text } from '@chakra-ui/react';
 
 type GifType = {
 	id: string;
@@ -17,10 +17,7 @@ const App = (): JSX.Element => {
 		const fetchGiphy = () => {
 			fetch(`.netlify/functions/fetchGiphy/?input=${input}`)
 				.then((response) => response.json())
-				.then((data) => {
-					console.log(data);
-					setGifs(data.data);
-				});
+				.then((data) => setGifs(data.data));
 		};
 
 		const timer = setTimeout(() => {
@@ -33,39 +30,33 @@ const App = (): JSX.Element => {
 	}, [input]);
 
 	return (
-		<Box w="100%" h="100%" bg="black">
-			<Center>
-				<Text
-					mt="48px"
-					bgGradient="linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(193,176,15,1) 0%, rgba(0,212,255,1) 100%)"
-					bgClip="text"
-					fontWeight="extrabold"
-					fontSize="32px"
-				>
-					Serverless Gifs App
-				</Text>
-			</Center>
-			<Center>
-				<Input
-					onChange={(event) => setInput(event.target.value)}
-					value={input}
-					mt="48px"
-					w="90%"
-					maxW="600px"
-					color="white"
-					size="lg"
-					focusBorderColor="green.300"
-					placeholder="search gifs"
-				/>
-			</Center>
-			<Center>
-				<Grid mt="80px" templateColumns="repeat(3, 1fr)" gap={8}>
-					{gifs.map((gif) => (
-						<GifCard key={gif.id} URL={gif.embed_url} />
-					))}
-				</Grid>
-			</Center>
-		</Box>
+		<Flex direction="column" align="center" w="100%" h="100%" bg="black">
+			<Text
+				mt="48px"
+				bgGradient="linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(193,176,15,1) 0%, rgba(0,212,255,1) 100%)"
+				bgClip="text"
+				fontWeight="extrabold"
+				fontSize="32px"
+			>
+				Serverless Gifs App
+			</Text>
+			<Input
+				onChange={(event) => setInput(event.target.value)}
+				value={input}
+				mt="48px"
+				w="90%"
+				maxW="600px"
+				color="white"
+				size="lg"
+				focusBorderColor="green.300"
+				placeholder="search gifs"
+			/>
+			<Grid mt="80px" templateColumns="repeat(3, 1fr)" gap={8}>
+				{gifs.map((gif) => (
+					<GifCard key={gif.id} URL={gif.embed_url} />
+				))}
+			</Grid>
+		</Flex>
 	);
 };
 
